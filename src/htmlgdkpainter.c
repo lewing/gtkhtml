db2dde61a9b15fb42caddb5edd963abd33f612d3
Larry Ewing <lewing@src.gnome.org>
2002-04-18 20:36:04 +0000

    i02-03-28  Larry Ewing  <lewing@ximian.com>
    	* htmlgdkpainter.c (draw_background): optimize the 1x1 pixel
    	background image into drawing a color or nothing.  This should
    	help over the wire drawing speed in some degenerate cases.
    
    svn path=/branches/gtkhtml-1-0-branch/; revision=4809

--- src/htmlgdkpainter.c
+++ src/htmlgdkpainter.c
@@ -658,6 +658,34 @@ draw_background (HTMLPainter *painter,
 	pw = gdk_pixbuf_get_width (pixbuf);
 	ph = gdk_pixbuf_get_height (pixbuf);
 
+	/* optimize out some special cases */
+	if (pw == 1 && ph == 1) {
+		GdkColor pixcol;
+		guchar *p;
+
+		p = gdk_pixbuf_get_pixels (pixbuf);
+		
+		if (!(gdk_pixbuf_get_has_alpha (pixbuf) && (p[3] < 0x80))) {
+			pixcol.red = p[0] * 0xff;
+			pixcol.green = p[1] * 0xff; 
+			pixcol.blue = p[2] * 0xff;
+			
+			alloc_color (painter, &pixcol);
+			color = &pixcol;
+		}
+
+		if (color) {
+			gdk_gc_set_foreground (gdk_painter->gc, color);
+			gdk_draw_rectangle (gdk_painter->pixmap, gdk_painter->gc,
+					    TRUE, x - gdk_painter->x1, y - gdk_painter->y1,
+					    width, height);
+		}	
+		
+		if (color == &pixcol)
+			free_color (painter, &pixcol);
+		return;
+	}
+
 	tile_width = (tile_x % pw) + width;
 	tile_height = (tile_y % ph) + height;
 
