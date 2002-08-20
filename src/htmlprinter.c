68667bb136232c351d5127d462822cceb6bdd8b8
Radek Doulik <rodo@src.gnome.org>
2002-08-20 19:25:45 +0000

    changes from HEAD
    svn path=/branches/gnome-2-port/; revision=5281

--- src/htmlprinter.c
+++ src/htmlprinter.c
@@ -719,9 +719,9 @@ alloc_font (HTMLPainter *painter, gchar *face, gdouble size, gboolean points, Gt
 	}
 
 	return font ? html_font_new (font,
-				     gnome_font_get_width_utf8_sized (font, " ", 1),
-				     gnome_font_get_width_utf8_sized (font, "\xc2\xa0", 2),
-				     gnome_font_get_width_utf8_sized (font, "\t", 1))
+				     SCALE_GNOME_PRINT_FONT_TO_ENGINE (gnome_font_get_width_utf8_sized (font, " ", 1)),
+				     SCALE_GNOME_PRINT_FONT_TO_ENGINE (gnome_font_get_width_utf8_sized (font, "\xc2\xa0", 2)),
+				     SCALE_GNOME_PRINT_FONT_TO_ENGINE (gnome_font_get_width_utf8_sized (font, "\t", 1)))
 		: NULL;
 }
 
