bf908e6c5267cb7b91b0ab90c833edf67e600beb
nobody <nobody@localhost>
2005-01-04 10:12:03 +0000

    This commit was manufactured by cvs2svn to create tag 'rtl-branch-merge-1'.
    svn path=/tags/rtl-branch-merge-1/; revision=7635

--- src/htmlengine.c
+++ src/htmlengine.c
@@ -289,12 +289,18 @@ html_element_new (HTMLEngine *e, const char *str) {
 
 			if (!g_hash_table_lookup (element->attributes, lower)) {
 				DE (g_print ("attrs (%s, %s)", attr[0], attr[1]));
-				g_hash_table_insert (element->attributes, lower, g_strdup (attr[1]));
-			} else
+				g_hash_table_insert (element->attributes, lower, attr[1]);
+
+				/* just free the array */
+				g_free (attr[0]);
+				g_free (attr);				
+			} else {
+				/* free the whole vector */
+				g_strfreev (attr);
 				g_free (lower);
+			}
 		}
 
-		g_strfreev (attr);
 	}
 	
 	return element;
@@ -5646,11 +5652,9 @@ replace (HTMLEngine *e)
 	html_search_push (e->search_info, e->cursor->object->parent);
 }
 
-gboolean
+void
 html_engine_replace_do (HTMLEngine *e, HTMLReplaceQueryAnswer answer)
 {
-	gboolean finished;
-
 	g_assert (e->replace_info);
 
 	switch (answer) {
@@ -5664,7 +5668,6 @@ html_engine_replace_do (HTMLEngine *e, HTMLReplaceQueryAnswer answer)
 		html_replace_destroy (e->replace_info);
 		e->replace_info = NULL;
 		html_engine_disable_selection (e);
-		finished = TRUE;
 		break;
 
 	case RQA_Replace:
@@ -5672,13 +5675,12 @@ html_engine_replace_do (HTMLEngine *e, HTMLReplaceQueryAnswer answer)
 		replace (e);
 		html_undo_level_end (e->undo);
 	case RQA_Next:
-		finished = !html_engine_search_next (e);
-		if (finished)
+		if (html_engine_search_next (e))
+			e->replace_info->ask (e, e->replace_info->ask_data);
+		else
 			html_engine_disable_selection (e);
 		break;
 	}
-
-	return finished;
 }
 
 /* spell checking */
